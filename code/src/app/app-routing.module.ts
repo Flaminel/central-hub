44d352835bccc5cd36bc36f08372ec9a28fa7283
Dana Sasaran <sasaran.dana@gmail.com>
2023-05-07 23:15:47 +0300

    Implement Auth0 login; Configure routes

--- code/src/app/app-routing.module.ts
+++ code/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './core/components/login/login.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
-    path: 'login',
-    component: LoginComponent,
+    path: '',
+    loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ];
 
---
+++ code/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  auth0: {
+    domain: 'central-hub.eu.auth0.com',
+    clientId: 'yFqvWMRGZ3VTNGBH6rUdVCetHdihxJfV',
+    authorizationParams: {
+      redirect_uri: 'http://localhost:4200',
+    },
+  },
+};
